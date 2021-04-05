4b8fdfd11d001bea8d8c29f8a506a29bf2263b03
Juan David Bravo <juanda.bravo98@gmail.com>
2021-04-05 15:29:45 -0500

    Menu behaviour and responsive. Js events still need to be fixed

--- scripts/main.js
+++ scripts/main.js
@@ -1,10 +1,26 @@
 window.addEventListener('load', () => {
 
-    const navItems = document.querySelectorAll('nav li');
+    const navItems = document.querySelectorAll('nav li p');
     const submenus = document.querySelectorAll('.submenu');
 
-    console.log(navItems);
+    navItems.forEach(item => {
+        item.addEventListener('click', openMenu);
+        item.addEventListener('focus', openMenu);
+    });
 
-    console.log(submenus);
+    submenus.forEach(submenu => {
+        submenu.addEventListener('mouseleave', closeMenu);
+        submenu.addEventListener('blur', closeMenu);
+    });
 
+    function openMenu(event) {
+        let target = event.target.closest('li');
+        target.classList.add('active');
+    }
+
+    function closeMenu(event) {
+        let target = event.target.closest('li');
+        let submenuLinks = Array.from(target.querySelectorAll('a'));
+        target.classList.remove('active');
+    }
 });
