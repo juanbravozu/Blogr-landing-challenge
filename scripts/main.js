05b4a3ee7fd9f6a50708b8911c40fb9dc1c8ee92
Juan David Bravo <juanda.bravo98@gmail.com>
2021-04-05 15:36:10 -0500

    Made responsive menu toggleable

--- scripts/main.js
+++ scripts/main.js
@@ -23,4 +23,18 @@ window.addEventListener('load', () => {
         let submenuLinks = Array.from(target.querySelectorAll('a'));
         target.classList.remove('active');
     }
+
+    const menuBtn = document.querySelector('nav button');
+    const menu = document.querySelector('.menu');
+
+    menuBtn.addEventListener('click', () => {
+        const btnImg = menuBtn.querySelector('img');
+        menu.classList.toggle('menu--active');
+
+        if(menu.classList.contains('menu--active')) {
+            btnImg.setAttribute('src', './images/icon-close.svg');
+        } else {
+            btnImg.setAttribute('src', './images/icon-hamburger.svg');
+        }        
+    });
 });
