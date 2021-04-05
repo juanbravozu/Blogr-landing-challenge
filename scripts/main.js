a69ff718219d3d5532b831eba01376e03edf89b7
Juan David Bravo <juanda.bravo98@gmail.com>
2021-04-05 15:53:08 -0500

    Fixed some of the events for the menu

--- scripts/main.js
+++ scripts/main.js
@@ -4,8 +4,9 @@ window.addEventListener('load', () => {
     const submenus = document.querySelectorAll('.submenu');
 
     navItems.forEach(item => {
-        item.addEventListener('click', openMenu);
-        item.addEventListener('focus', openMenu);
+        item.addEventListener('click', toggleMenu);
+        //item.addEventListener('focus', openMenu);
+        item.addEventListener('blur', closeMenu);
     });
 
     submenus.forEach(submenu => {
@@ -14,6 +15,10 @@ window.addEventListener('load', () => {
     });
 
     function openMenu(event) {
+        navItems.forEach(item => {
+            item.closest('li').classList.remove('active');
+        });
+
         let target = event.target.closest('li');
         target.classList.add('active');
     }
@@ -22,6 +27,19 @@ window.addEventListener('load', () => {
         let target = event.target.closest('li');
         let submenuLinks = Array.from(target.querySelectorAll('a'));
         target.classList.remove('active');
+    }
+
+    function toggleMenu(event) {
+        let target = event.target.closest('li');
+        const isActive = target.classList.contains('active');
+        console.log(target.classList);
+        navItems.forEach(item => {
+            item.closest('li').classList.remove('active');
+        });
+        
+        console.log(isActive);
+
+        if(!isActive) target.classList.add('active');
     }
 
     const menuBtn = document.querySelector('nav button');
@@ -33,8 +51,10 @@ window.addEventListener('load', () => {
 
         if(menu.classList.contains('menu--active')) {
             btnImg.setAttribute('src', './images/icon-close.svg');
+            document.querySelector('body').style.overflowX = 'hidden';
         } else {
             btnImg.setAttribute('src', './images/icon-hamburger.svg');
+            document.querySelector('body').style.overflowX = 'auto';
         }        
     });
 });
